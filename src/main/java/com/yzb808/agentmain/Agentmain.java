7274794c2f09c35547460c8e86906d7e69dd87c3
XuYun <yzb808@126.com>
2016-04-25 15:06:39 +0800

    新增注释

--- src/main/java/com/yzb808/agentmain/Agentmain.java
+++ src/main/java/com/yzb808/agentmain/Agentmain.java
@@ -10,8 +10,8 @@ public class Agentmain {
 
 	public static void agentmain(String args, Instrumentation inst) {
 		System.out.println("MyAgentMain agentmain attach...");
-		// inst中包含所有已经加载了的类
-		for (Class clazz : inst.getAllLoadedClasses()) {
+		// inst中包含所有已经加载了的类，这里也能够测试说明，用户自定的类是在用户使用该类的时候才被classLoader加载进来
+		for (Class<?> clazz : inst.getAllLoadedClasses()) {
 			if (clazz.getName().startsWith("com.yzb808.main")) {
 				System.out.println(clazz.getName());
 			}
--- src/main/java/com/yzb808/main/Main.java
+++ src/main/java/com/yzb808/main/Main.java
@@ -9,8 +9,6 @@ public class Main {
 	private static String jarPath = "E:/eclipse/workSpace/agentmain/target/agentmain-0.0.1.jar";
 
 	public static void main(String[] args) throws Exception {
-		Student student = new Student();
-		System.out.println(student);
 		System.out.println("main begin");
 		String pid = getJVMPid();
 		VirtualMachine vm = VirtualMachine.attach(pid);
@@ -30,23 +28,4 @@ public class Main {
 		}
 		return pid;
 	}
-	
-	private static class Student {
-		private String name;
-		private Integer age;
-		
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public Integer getAge() {
-			return age;
-		}
-		public void setAge(Integer age) {
-			this.age = age;
-		}
-	}
-
 }
